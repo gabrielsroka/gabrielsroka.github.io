733be019d39404bb8bd7742d3959c8cb7c3c5ea3
gabrielsroka <gabrielsroka@gmail.com>
2015-09-21 13:59:10 -0700

    Update saml.js

--- saml.js
+++ saml.js
@@ -9,25 +9,30 @@
             var response = (match[1] == "SAMLResponse" ? atob(value) : value).replace(/\n/g, "");
             var highlight = "style='background-color: yellow'";
             response = response.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/&gt;&lt;/g, "&gt;\n&lt;").
-                                replace(/((SignatureValue|X509Certificate)&gt;.{80})(.*)&lt;/g, "$1<span title='$3' " + highlight + ">...</span>&lt;").
-                                replace(/((Address|Issuer|NameID|NameIdentifier|AttributeValue|Audience|Destination|Recipient)(.*&gt;|="))(.*?)(&lt;|")/g, "$1<span " + highlight + ">$4</span>$5");
+                replace(/((SignatureValue|X509Certificate)&gt;.{80})(.*)&lt;/g, "$1<span title='$3' " + highlight + ">...</span>&lt;").
+                replace(/((Address|Issuer|NameID|NameIdentifier|AttributeValue|Audience|Destination|Recipient)(.*&gt;|="))(.*?)(&lt;|")/g, "$1<span " + highlight + ">$4</span>$5");
             results.innerHTML = "<pre>" + indentXml(response, 4) + "</pre>";
         } else {
             results.innerHTML = "Error";
         }
     }
     var div = document.body.appendChild(document.createElement("div"));
-    div.innerHTML = "<a onclick='document.body.removeChild(this.parentNode)'>&nbsp;SAML Response - X</a>" +
-                    "<div id=results><form id=form><input id=url style='width:700px;' placeholder='URL'> <input type=submit value=Go></form></div>";
+    div.innerHTML = "<a onclick='document.body.removeChild(this.parentNode)'>&nbsp;SAML Response - X</a>";
     div.style.position = "absolute";
     div.style.zIndex = "1000";
     div.style.left = "4px";
     div.style.top = "4px";
     div.style.backgroundColor = "white";
-    var url = document.getElementById("url");
+    var results = div.appendChild(document.createElement("div"));
+    var form = results.appendChild(document.createElement("form"));
+    var url = form.appendChild(document.createElement("input"));
+    url.style.width = "700px";
+    url.placerholder = "URL";
     url.focus();
-    var results = document.getElementById("results");
-    document.getElementById("form").onsubmit = function () {
+    var input = form.appendChild(document.createElement("input"));
+    input.type = "submit";
+    input.value = "Go";
+    form.onsubmit = function () {
         results.innerHTML = "Loading . . .";
         var request = new XMLHttpRequest();
         request.open("get", url.value);
@@ -37,18 +42,16 @@
         request.send();
         return false;
     };
-    function indentXml(xml, tabs) {
+    function indentXml(xml, size) {
         var lines = xml.split("\n");
-        var tab = 0;
+        var level = 0;
         for (var li = 0; li < lines.length; li++) {
             var line = lines[li];
             var end = line.match("&lt;/");
             var empty = line.match("/&gt;") || line.match(/&gt;.*&gt;/);
-            var indent  = !end && !empty;
-            var outdent =  end && !empty;
-            if (outdent) tab--;
-            lines[li] = " ".repeat(tabs * tab) + line;
-            if (indent) tab++;
+            if (end && !empty) level--;
+            lines[li] = " ".repeat(size * level) + line;
+            if (!end && !empty) level++;
         }
         return lines.join("\n");
     }

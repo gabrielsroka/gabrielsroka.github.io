0723aa91b0232a2969f278d3b18df2b4af8a4fae
gabrielsroka <gabrielsroka@gmail.com>
2016-12-30 11:05:04 -0800

    Create oktaAPIsettings.js

---
+++ OktaAddPersonChrome/oktaAPIsettings.js
@@ -0,0 +1,5 @@
+// apitoken comes from Okta Admin > Security > API > Create Token
+// see http://developer.okta.com/docs/api/getting_started/getting_a_token.html
+
+var baseurl = "https://YOUR_ORG.oktapreview.com";
+var apitoken = "YOUR_TOKEN";

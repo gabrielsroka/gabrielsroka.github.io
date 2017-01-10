9f4b0ef4b9c16a964fab6eb1fc7cbd96ddce4329
gabrielsroka <gabrielsroka@gmail.com>
2017-01-09 18:33:01 -0800

    Delete oktaAPIsettings.js

--- oktaAPIsettings.js
+++
@@ -1,5 +0,0 @@
-// apitoken comes from Okta Admin > Security > API > Create Token
-// see http://developer.okta.com/docs/api/getting_started/getting_a_token.html
-
-var baseurl = "https://YOUR_ORG.oktapreview.com";
-var apitoken = "YOUR_TOKEN";

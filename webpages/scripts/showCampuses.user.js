88a87f993e29d11c20a335f95011230d7dba44d9
gabrielsroka <gabrielsroka@gmail.com>
2017-04-25 14:29:00 -0700

    Add files via upload

---
+++ webpages/scripts/showCampuses.user.js
@@ -0,0 +1,16 @@
+﻿//Greasemonkey:
+
+// ==UserScript==
+// @name           InC Parts
+// @namespace      http://webpages.charter.net/gabrielsroka
+// @include        http://www.incommonfederation.org/participants/
+// @include        http://www.incommon.org/participants/
+// ==/UserScript==
+
+(function () {
+	var script = document.body.appendChild(document.createElement("script"));
+	script.src = "http://webpages.charter.net/gabrielsroka/scripts/showCampuses.js";
+	script.type = "text/javascript";
+	script.id = "gabrielsroka_showCampuses";
+}
+)();

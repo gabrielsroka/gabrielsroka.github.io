4669f625a3ef56f39695c61f7676836ee9ebc564
gabrielsroka <gabrielsroka@gmail.com>
2017-05-16 10:16:25 -0700

    Update callOktaAPI.js

--- callOktaAPI.js
+++ callOktaAPI.js
@@ -117,7 +117,7 @@ script.src="https://gabrielsroka.github.io/callOktaAPI.js";script.onload=functio
     }
     function createDiv(title) {
         var div = document.body.appendChild(document.createElement("div"));
-        div.innerHTML = "<a onclick='document.body.removeChild(this.parentNode)'>" + title + " - X</a> " +
+        div.innerHTML = "<a onclick='document.body.removeChild(this.parentNode)'>" + title + " - close</a> " +
             "<a href='https://gabrielsroka.github.io/' target='_blank'>?</a>";
         div.style.position = "absolute";
         div.style.zIndex = "1000";

d882cb7ab64a6bc039296a1a8445d75cc3a70bb8
gabrielsroka <gabrielsroka@gmail.com>
2017-05-23 13:20:05 -0700

    Update exportObjects.js

--- exportObjects.js
+++ exportObjects.js
@@ -23,7 +23,7 @@
                 getObjects("App Groups", "/apps/" + appid + "/groups?limit=50", "id,licenses", function (appgroup) {
                     callAPI("/groups/" + appgroup.id, function () {
                         var group = JSON.parse(this.responseText);
-                        groups.push(group.profile.name + "," + (appgroup.profile.licenses ? appgroup.profile.licenses.join(";") : ""))+
+                        groups.push(group.profile.name + "," + (appgroup.profile.licenses ? appgroup.profile.licenses.join(";") : "") +
                             "," + (appgroup.profile.roles ? appgroup.profile.roles.join(";") : ""));
                         if (groups.length == total) {
                             console.log("groups");

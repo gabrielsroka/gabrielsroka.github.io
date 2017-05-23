f0731083bf53c04dae43b60bb00a548a7f66e6b8
gabrielsroka <gabrielsroka@gmail.com>
2017-05-23 13:17:08 -0700

    Update exportObjects.js

--- exportObjects.js
+++ exportObjects.js
@@ -23,7 +23,8 @@
                 getObjects("App Groups", "/apps/" + appid + "/groups?limit=50", "id,licenses", function (appgroup) {
                     callAPI("/groups/" + appgroup.id, function () {
                         var group = JSON.parse(this.responseText);
-                        groups.push(group.profile.name + "," + (appgroup.profile.licenses ? appgroup.profile.licenses.join(";") : ""));
+                        groups.push(group.profile.name + "," + (appgroup.profile.licenses ? appgroup.profile.licenses.join(";") : "")) +
+                            "," + (appgroup.profile.roles ? appgroup.profile.roles.join(";") : ""));
                         if (groups.length == total) {
                             console.log("groups");
                             for (var g = 0; g < groups.length; g++) {
@@ -31,7 +32,8 @@
                             }
                         }
                     });
-                    return appgroup.id + "," + (appgroup.profile.licenses ? appgroup.profile.licenses.join(";") : "");
+                    return appgroup.id + "," + (appgroup.profile.licenses ? appgroup.profile.licenses.join(";") : "") +
+                        "," + (appgroup.profile.roles ? appgroup.profile.roles.join(";") : ""));
                 });
             };
             results.appendChild(document.createElement("br"));

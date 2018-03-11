5840dd0d67b4d1ada8a308ecd166bf4164238296
gabrielsroka <gabrielsroka@gmail.com>
2018-03-11 14:28:02 -0700

    Create admin-mobile.user.js

---
+++ admin-mobile.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name           Okta Admin Mobile
+// @namespace      https://gabrielsroka.github.io/
+// @description    Okta Admin Mobile
+// @version        2018.3.11.1400
+// @include        https://*.oktapreview.com/*
+// @require        https://code.jquery.com/jquery-1.12.4.min.js
+// ==/UserScript==
+
+(function () {
+	document.body.appendChild(document.createElement("script")).src = "https://gabrielsroka.github.io/admin-mobile.js";
+}
+)();

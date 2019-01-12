6ee092d376ea93d40e5be818f6ee6bce88ba075d
Gabriel Sroka <gabrielsroka@gmail.com>
2019-01-12 10:56:32 -0800

    Create rockstar.user.js

---
+++ rockstar/rockstar.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name           rockstar
+// @namespace      https://gabrielsroka.github.io/
+// @description    Export Okta Users, Groups, etc. to CSV. Show SAML assertion. And so much more.
+// @version        2019.1.13.1100
+// @include        https://*.oktapreview.com/*
+// @require        https://code.jquery.com/jquery-1.12.4.min.js
+// ==/UserScript==
+
+(function () {
+    document.body.appendChild(document.createElement("script")).src = "https://gabrielsroka.github.io/rockstar/rockstar.js";
+}
+)();

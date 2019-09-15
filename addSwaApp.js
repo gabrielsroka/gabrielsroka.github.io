e05e98bdca03230cc603b046aeec16eed168d443
Gabriel Sroka <gabrielsroka@gmail.com>
2019-09-14 17:50:29 -0700

    use async await

--- addSwaApp.js
+++ addSwaApp.js
@@ -11,63 +11,53 @@ Usage:
 */
 
 (function () {
-    var me;
-    var div = $(createDiv("Add App"));
-    div.html("<form><table><tr><td>Label<td><input class=label style='width: 300px'>" + 
+    var popup = createPopup("Add SWA App");
+    var form = $("<form><table><tr><td>Label<td><input class=label style='width: 300px'>" + 
         "<tr><td>Login URL<td><input class=loginUrl value='https://LOGIN.oktapreview.com' style='width: 300px'></table>" + 
-        "<button type=submit>Add</button></form>");
-    div.find("button").click(function () {
-        $.get("/api/v1/users/me").then(function (user) {
-            me = user;
-            var app = {
-                label: div.find("input.label").val(),
-                settings: {
-                    signOn: {
-                        loginUrl: div.find("input.loginUrl").val()
-                    }
-                },
-                signOnMode: "AUTO_LOGIN",
-                visibility: {
-                    autoSubmitToolbar: false
+        "<button type=submit>Add</button></form>").appendTo(popup);
+    form.submit(async (event) => {
+        event.preventDefault();
+        popup.html("Getting user...");
+        var me = await $.get("/api/v1/users/me");
+        var app = {
+            label: form.find("input.label").val(),
+            settings: {
+                signOn: {
+                    loginUrl: form.find("input.loginUrl").val()
                 }
-            };
-            div.html("Adding App...");
-            // https://developer.okta.com/docs/api/resources/apps#add-custom-swa-application
-            return postJson({
-                url: "/api/v1/apps",
-                data: app
-            });
-        }).then(function (app) {
-            var appUser = {
-                id: me.id,
-                scope: "USER",
-            };
-            div.html("Assigning User...");
-            // https://developer.okta.com/docs/api/resources/apps#assign-user-to-application-for-sso
-            return postJson({
-                url: "/api/v1/apps/" + app.id + "/users",
-                data: appUser
-            });
-        }).then(function (appUser) {
-            div.html("Done.");
+            },
+            signOnMode: "AUTO_LOGIN",
+            visibility: {
+                autoSubmitToolbar: false
+            }
+        };
+        popup.html("Adding app...");
+        // https://developer.okta.com/docs/reference/api/apps/#add-custom-swa-application
+        app = await postJson({
+            url: "/api/v1/apps",
+            data: app
         });
-        return false; // Cancel form.
+        var appUser = {
+            id: me.id,
+            scope: "USER"
+        };
+        popup.html("Assigning user to app...");
+        // https://developer.okta.com/docs/reference/api/apps/#assign-user-to-application-for-sso
+        await postJson({
+            url: "/api/v1/apps/" + app.id + "/users",
+            data: appUser
+        });
+        popup.html(`Added app <a href='/admin/app/${app.name}/instance/${app.id}/'>${app.label}</a>.`);
     });
     function postJson(settings) {
         settings.contentType = "application/json";
         settings.data = JSON.stringify(settings.data);
         return $.post(settings);
     }
-    function createDiv(title) {
-        var div = document.body.appendChild(document.createElement("div"));
-        div.innerHTML = "<a onclick='document.body.removeChild(this.parentNode)'>" + title + " - close</a> " +
-            "<a href='https://gabrielsroka.github.io/' target='_blank'>?</a>";
-        div.style.position = "absolute";
-        div.style.zIndex = "1000";
-        div.style.left = "4px";
-        div.style.top = "4px";
-        div.style.backgroundColor = "white";
-        div.style.padding = "8px";
-        return div.appendChild(document.createElement("div"));
+    function createPopup(title) {
+        var popup = $(`<div style='position: absolute; z-index: 1000; left: 4px; top: 4px; background-color: white; padding: 8px; border: 1px solid #ddd;'>` +
+            `${title}<div style='display: block; float: right;'><a href='https://gabrielsroka.github.io' target='_blank' rel='noopener'>&nbsp;?&nbsp;</a> ` + 
+            `<a onclick='document.body.removeChild(this.parentNode.parentNode)' style='cursor: pointer'>&nbsp;X&nbsp;</a></div><br><br></div>`).appendTo(document.body);
+        return $("<div></div>").appendTo(popup);
     }
 })();

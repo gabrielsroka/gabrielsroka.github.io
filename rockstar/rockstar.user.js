8d16f5f64edeccbc5663a6cfe95bed0b8f58ca4e
Gabriel Sroka <gabrielsroka@gmail.com>
2020-02-21 17:19:57 -0800

    Update rockstar.user.js

--- rockstar/rockstar.user.js
+++ rockstar/rockstar.user.js
@@ -2,7 +2,9 @@
 // @name           rockstar
 // @namespace      https://gabrielsroka.github.io/
 // @description    Export Okta Users, Groups, etc. to CSV. Show SAML assertion. And so much more.
-// @version        2019.1.13.1100
+// @version        2020.2.21.1100
+// @include        https://*.okta.com/*
+// @include        https://*.okta-emea.com/*
 // @include        https://*.oktapreview.com/*
 // @require        https://code.jquery.com/jquery-1.12.4.min.js
 // ==/UserScript==

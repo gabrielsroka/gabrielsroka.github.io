9c4b1e2c80827b132f1be1f810de880421f79bf1
Gabriel Sroka <gabrielsroka@gmail.com>
2021-12-11 11:54:27 -0800

    Update SearchGroups.js

--- SearchGroups.js
+++ SearchGroups.js
@@ -1,5 +1,7 @@
 javascript:
 /*
+Bookmark name: /Search Groups#
+
 Setup:
 Copy this code to the browser console, or, if using Chrome, to a Snippet. For example:
 1. Press F12 (Windows) to open DevTools.
@@ -14,24 +16,23 @@ Usage:
 3. Run the code. If using a Snippet, there's a Run button on the bottom right, or press Ctrl+Enter (Windows).
 4. Look for the popup window in the upper-left corner of your browser.
 */
-
 (async function () {
     const popup = createPopup('Search 10,000 Groups with Name Containing');
-    const form = $('<form>Name <input class=name style="width: 250px"> ' + 
-        '<button type=submit disabled>Search</button></form><br><div class=results>Loading...</div>').appendTo(popup);
+    const form = $('<form>Name <input class=name style="width: 250px"> <button type=submit disabled>Search</button></form><br>' + 
+        '<div class=results>Loading...</div>').appendTo(popup);
+    const groups = await $.getJSON('/api/v1/groups');
+    popup.find('button').prop('disabled', false);
+    popup.find('div.results').html('');
     form.find('input.name').focus();
     form.submit(event => {
         event.preventDefault();
         const re = new RegExp(form.find('input.name').val(), 'i');
         const found = groups
             .filter(group => re.test(group.profile.name))
-            .map(group => group.profile.name.link('/admin/group/' + group.id));
-        const results = found.length > 0 ? found.join('<br>') : 'Not found';
-        popup.find('div.results').html(results);
+            .map(group => group.profile.name.link('/admin/group/' + group.id) + '<br>')
+            .join('');
+        popup.find('div.results').html(found || 'Not found');
     });
-    const groups = await $.getJSON('/api/v1/groups');
-    popup.find('button').prop('disabled', false);
-    popup.find('div.results').html('');
 
     function createPopup(title) {
         const popup = $(`<div style='position: absolute; z-index: 1000; top: 0px; max-height: calc(100% - 28px); max-width: calc(100% - 28px); padding: 8px; margin: 4px; overflow: auto; ` +

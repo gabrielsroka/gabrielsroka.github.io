f1accf2c3c475a977a295aa288024339e5a8004f
Gabriel Sroka <gabrielsroka@gmail.com>
2022-08-04 20:09:16 -0700

    Update console.js

--- console.js
+++ console.js
@@ -1,32 +1,37 @@
 javascript:
-/* Bookmarklet name: /console# */
+/* Bookmarklet name: /console#
+url: https://github.com/gabrielsroka/gabrielsroka.github.io/blob/master/console.js */
 (function () {
  div = document.body.appendChild(document.createElement('div'));
  div.innerHTML = `<button id=run>Run</button>
   <button onclick=document.body.removeChild(this.parentNode)>Close</button><br>
   <textarea id=editor style='width: 100%; height: 300px; font-family: monospace;' spellcheck=false autocapitalize=none>
 f = 'filter=profile.lastName eq "Doe"'\n
-url = \`/api/v1/users?\${f}&limit=2\`\n
-while (url) {\n
-  r = await get(url)\n
-  us = await r.json()\n
-  for (u of us)\n
-    log(u.profile.login)\n
-  url = r.nextUrl\n
-}\n</textarea><br>
+url = '/api/v1/users?limit=2&' + f\n
+for await (users of getPages(url)) {\n
+  for (user of users)\n
+    log(user.id, user.profile.login)\n
+}</textarea><br>
   <textarea id=debug style='width: 100%; height: 300px; font-family: monospace;' spellcheck=false autocapitalize=none></textarea>`;
  div.style.cssText = 'position: absolute; padding: 8px; width: 100%; top: 0px; background-color: white; z-index: 1001;';
  run.onclick = function () {
   debug.value = '';
   eval('(async function () {' + editor.value + '})()');
  };
- function log(s) {
-  debug.value += s + '\n';
+ function log(...s) {
+  debug.value += s.join(' ') + '\n';
  }
- async function get(url) {
-  const r = await fetch(url);
-  r.nextUrl = r.headers.get('link')?.match('<https://[^/]+(/[^>]+)>; rel="next"')?.[1];
-  return r;
+ const get = fetch;
+ async function* getPages(url) {
+  while (url) {
+   const response = await fetch(url);
+   const page = await response.json();
+   yield page;
+   url = response.headers.get('link')?.match('<https://[^/]+(/[^>]+)>; rel="next"')?.[1];
+  }
+ }
+ async function put(url, data) {
+  return await $.ajax(url, {method: 'put', data: JSON.stringify(data), contentType: 'application/json'});
  }
  async function remove(url) {
   return await $.ajax(url, {method: 'delete'});

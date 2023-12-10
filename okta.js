5416819d3293b579b755edf4740dbf3f7d7a50a2
Gabriel Sroka <gabrielsroka@gmail.com>
2023-12-09 20:29:17 -0800

    Update okta.js

--- okta.js
+++ okta.js
@@ -6,6 +6,8 @@ let _options;
 export function init(url, options = {}) {
     baseUrl = url;
     _options = options;
+    _options.headers = _options.headers || {};
+    _options.headers['Content-Type'] = 'application/json';
 }
 
 export async function get(path) {
@@ -21,6 +23,5 @@ export async function put(path, body) {
 }
 
 async function fetchOkta(path, init) {
-    init.headers['Content-Type'] = 'application/json';
     return fetch(baseUrl + path, {..._options, ...init});
 }

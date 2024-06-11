89277e3f685a9489f4dc06aba35424266e030cd5
Gabriel Sroka <gabrielsroka@gmail.com>
2024-06-11 11:50:51 -0700

    Update rockstar.user.js

--- rockstar/rockstar.user.js
+++ rockstar/rockstar.user.js
@@ -2,7 +2,7 @@
 // @name           rockstar
 // @namespace      https://gabrielsroka.github.io/
 // @description    Export Okta Users, Groups, etc. to CSV. Show SAML assertion. And so much more.
-// @version        2023.11.14.1600
+// @version        2024.06.11.1100
 // @include        https://*.okta.com/*
 // @include        https://*.okta-emea.com/*
 // @include        https://*.okta-gov.com/*
@@ -11,6 +11,9 @@
 // ==/UserScript==
 
 (function () {
-    document.body.appendChild(document.createElement("script")).src = "https://gabrielsroka.github.io/rockstar/rockstar.js";
+  document.body.appendChild(document.createElement("script")).src = "https://gabrielsroka.github.io/rockstar/rockstar.js";
+  const link = document.head.appendChild(document.createElement('link'));
+  link.href = 'https://gabrielsroka.github.io/rockstar/rockstar.css'; 
+  link.rel = 'stylesheet';
 }
 )();

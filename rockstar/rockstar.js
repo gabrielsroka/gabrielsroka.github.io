fd50c0f2d61e2500e895312b1d40b8834b2966b2
Remi Koenig <remi.koenig@gmail.com>
2024-09-20 10:41:57 +0200

    remove refresh buttons

--- rockstar/rockstar.js
+++ rockstar/rockstar.js
@@ -1323,56 +1323,32 @@
     }
 
     async function createObjectHistoryForUser(user) {
-        const {logListPopup, searchInputHTML} = createPopupWithSearch(`User History for ${user.profile.firstName + " " + user.profile.lastName + " (" + user.id + ")"}`, "Event name", `${user.id}`);
+        const {logListPopup, searchInputHTML} = createPopupWithSearch(`User History for ${user.profile.firstName + " " + user.profile.lastName + " (" + user.id + ")"}`, "Search event name...", `${user.id}`);
         logListPopup.parent().attr('class', `historyListPopup`);
-        const svg = $(`<div class="rockstarButtons" style="display:inline; margin-left: 10px"><a class="refreshHistory"><svg class="rockstarButtons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" stroke="currentcolor">` +
-            `<path d="M 20 4 C 15.054688 4 11 8.054688 11 13 L 11 35.5625 L 5.71875 30.28125 L 4.28125 31.71875 L 11.28125 38.71875 L 12 39.40625 L 12.71875 38.71875 L 19.71875 31.71875 L 18.28125 30.28125 L 13 35.5625 L 13 13 C 13 9.144531 16.144531 6 20 6 L 31 6 L 31 4 Z M 38 10.59375 L 37.28125 11.28125 L 30.28125 18.28125 L 31.71875 19.71875 L 37 14.4375 L 37 37 C 37 40.855469 33.855469 44 30 44 L 19 44 L 19 46 L 30 46 C 34.945313 46 39 41.945313 39 37 L 39 14.4375 L 44.28125 19.71875 L 45.71875 18.28125 L 38.71875 11.28125 Z"></path>` +
-            `</svg></a></div>`);
-        logListPopup.append(svg);
         const sinceDate = new Date();
         sinceDate.setDate(sinceDate.getDate() - 90);
         const popupConfig = logListPopups['userObjectHistory'];
         let historyTable = displayHistoryResultTable(popupConfig, logListPopup, searchInputHTML, user.id);
-        logListPopup.find('.refreshHistory').click(async function() {
-            historyTable.find('tbody').empty();
-            await fetchMoreHistory(`/api/v1/logs?since=${sinceDate.toISOString()}&limit=10&filter=${popupConfig.oktaFilter.replaceAll("${userId}", user.id)}&sortOrder=DESCENDING`, 10, historyTable);
-        });
         await fetchMoreHistory(`/api/v1/logs?since=${sinceDate.toISOString()}&limit=10&filter=${popupConfig.oktaFilter.replaceAll("${userId}", user.id)}&sortOrder=DESCENDING`, 10, historyTable);
     }
 
     async function createObjectHistoryForGroup(group) {
-        const {logListPopup, searchInputHTML} = createPopupWithSearch(`Group History for ${group.profile.name + " (" + group.id + ")"}`, "Event name", `${group.id}`);
+        const {logListPopup, searchInputHTML} = createPopupWithSearch(`Group History for ${group.profile.name + " (" + group.id + ")"}`, "Search event name...", `${group.id}`);
         logListPopup.parent().attr('class', `historyListPopup`);
-        const svg = $(`<div class="rockstarButtons" style="display:inline; margin-left: 10px"><a class="refreshHistory"><svg class="rockstarButtons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" stroke="currentcolor">` +
-            `<path d="M 20 4 C 15.054688 4 11 8.054688 11 13 L 11 35.5625 L 5.71875 30.28125 L 4.28125 31.71875 L 11.28125 38.71875 L 12 39.40625 L 12.71875 38.71875 L 19.71875 31.71875 L 18.28125 30.28125 L 13 35.5625 L 13 13 C 13 9.144531 16.144531 6 20 6 L 31 6 L 31 4 Z M 38 10.59375 L 37.28125 11.28125 L 30.28125 18.28125 L 31.71875 19.71875 L 37 14.4375 L 37 37 C 37 40.855469 33.855469 44 30 44 L 19 44 L 19 46 L 30 46 C 34.945313 46 39 41.945313 39 37 L 39 14.4375 L 44.28125 19.71875 L 45.71875 18.28125 L 38.71875 11.28125 Z"></path>` +
-            `</svg></a></div>`);
-        logListPopup.append(svg);
         const sinceDate = new Date();
         sinceDate.setDate(sinceDate.getDate() - 90);
         const popupConfig = logListPopups['groupObjectHistory'];
         let historyTable = displayHistoryResultTable(logListPopups['groupObjectHistory'], logListPopup, searchInputHTML, group.id);
-        logListPopup.find('.refreshHistory').click(async function() {
-            historyTable.find('tbody').empty();
-            await fetchMoreHistory(`/api/v1/logs?since=${sinceDate.toISOString()}&limit=10&filter=${popupConfig.oktaFilter.replaceAll("${groupId}", group.id)}&sortOrder=DESCENDING`, 10, historyTable);
-        });
         await fetchMoreHistory(`/api/v1/logs?since=${sinceDate.toISOString()}&limit=10&filter=${popupConfig.oktaFilter.replaceAll("${groupId}", group.id)}&sortOrder=DESCENDING`, 10, historyTable);
     }
 
     async function createObjectHistoryForApp(app) {
-        const {logListPopup, searchInputHTML} = createPopupWithSearch(`App History for ${app.label + " (" + app.id + ")"}`, "Event name", `${app.id}`);
+        const {logListPopup, searchInputHTML} = createPopupWithSearch(`App History for ${app.label + " (" + app.id + ")"}`, "Search event name...", `${app.id}`);
         logListPopup.parent().attr('class', `historyListPopup`);
-        const svg = $(`<div class="rockstarButtons" style="display:inline; margin-left: 10px"><a class="refreshHistory"><svg class="rockstarButtons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" stroke="currentcolor">` +
-            `<path d="M 20 4 C 15.054688 4 11 8.054688 11 13 L 11 35.5625 L 5.71875 30.28125 L 4.28125 31.71875 L 11.28125 38.71875 L 12 39.40625 L 12.71875 38.71875 L 19.71875 31.71875 L 18.28125 30.28125 L 13 35.5625 L 13 13 C 13 9.144531 16.144531 6 20 6 L 31 6 L 31 4 Z M 38 10.59375 L 37.28125 11.28125 L 30.28125 18.28125 L 31.71875 19.71875 L 37 14.4375 L 37 37 C 37 40.855469 33.855469 44 30 44 L 19 44 L 19 46 L 30 46 C 34.945313 46 39 41.945313 39 37 L 39 14.4375 L 44.28125 19.71875 L 45.71875 18.28125 L 38.71875 11.28125 Z"></path>` +
-            `</svg></a></div>`);
-        logListPopup.append(svg);
         const sinceDate = new Date();
         sinceDate.setDate(sinceDate.getDate() - 90);
         const popupConfig = logListPopups['appObjectHistory'];
         let historyTable = displayHistoryResultTable(logListPopups['appObjectHistory'], logListPopup, searchInputHTML, app.id);
-        logListPopup.find('.refreshHistory').click(async function() {
-            historyTable.find('tbody').empty();
-            await fetchMoreHistory(`/api/v1/logs?since=${sinceDate.toISOString()}&limit=10&filter=${popupConfig.oktaFilter.replaceAll("${appId}", app.id)}&sortOrder=DESCENDING`, 10, historyTable);
-        });
         await fetchMoreHistory(`/api/v1/logs?since=${sinceDate.toISOString()}&limit=10&filter=${popupConfig.oktaFilter.replaceAll("${appId}", app.id)}&sortOrder=DESCENDING`, 10, historyTable);
     }
 

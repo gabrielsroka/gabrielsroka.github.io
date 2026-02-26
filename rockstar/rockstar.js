8bf1dc0b8ca28289604ad68ec905d0b5badc8b7b
Gabriel Sroka <gabrielsroka@gmail.com>
2026-02-26 12:37:31 -0800

    Update rockstar.js

--- rockstar/rockstar.js
+++ rockstar/rockstar.js
@@ -1152,7 +1152,7 @@
                 saveButton.attr('disabled', true);
             });
 
-        $("<input type='text' id='backuptaUrlInput' placeholder='https://...'>")
+        $("<input type='text' id=backuptaUrlInput placeholder='https://...'>")
             .val(localStorage.backuptaBaseUrl) // Set the default value
             .appendTo(backuptaUrlDiv)
             .keyup(function () {
@@ -1165,7 +1165,7 @@
     function createPopupWithSearch(popupTitle, searchPlaceholder) {
         const logListPopup = createPopup(popupTitle);
         logListPopup.parent().attr('id', 'logListPopup');
-        const searchInputHTML = `<input type='text' id='userSearch' style='margin-bottom: 10px' placeholder='${searchPlaceholder}'>`;
+        const searchInputHTML = `<input type='text' id=userSearch style='margin-bottom: 10px' placeholder='${searchPlaceholder}'>`;
         logListPopup.prepend(searchInputHTML);
         return {logListPopup, searchInputHTML};
     }
@@ -1198,7 +1198,7 @@
             if (log.target && log.target.length > 0) {
                 log.target.forEach(target => {
                     targetHTML += `<tr class='data-list-item' data-displayname='${e(target.displayName)}'>` +
-                        `<td><input type='checkbox' id='${e(target.id)}'>` +
+                        `<td><input type=checkbox id='${e(target.id)}'>` +
                         `<td><label for='${e(target.id)}'>${e(target.displayName)}</label>` +
                         `<td>${e(target.id)}` +
                         `<td>${e(target.type)}` +
@@ -1217,12 +1217,12 @@
         let targetHTML = "<table class='data-list-table rockstar' style='border: 1px solid #ddd;'><thead>" +
             "<tr><th>&nbsp;<th>Display Name<th>ID<th>Type<th>Deleted By<th>Deleted On" +
             "<tbody></tbody></table>" +
-            "<div style='float: right'><a href='#' id='showMore'>Show more</a></div>" +
-            "<div style='margin-top: 15px;'><button id='btnRestore'>Restore with Backupta</button></div>";
+            "<div style='float: right'><a href='#' id=showMore>Show more</a></div>" +
+            "<div style='margin-top: 15px;'><button id=btnRestore>Restore with Backupta</button></div>";
         logListPopup.html(targetHTML);
         $(logListPopup).prepend(searchInputHTML);
 
-        btnRestore.onclick = function () {
+        document.getElementById('btnRestore').onclick = function () {
             var baseUrl = localStorage.backuptaBaseUrl;
             if (!baseUrl) {
                 settings();
@@ -1427,16 +1427,20 @@
         function toggleSide() {
             popup.toggleClass('rs_toggle');
         }
-        const popup = $(`<div style='position: absolute; z-index: 1000; top: 0px; max-height: calc(100% - 28px); max-width: calc(100% - 28px); padding: 8px; margin: 4px; overflow: auto; ` +
-                `background-color: white; border: 1px solid #ddd;'>` +
-                `${main ? "<span class=title><span style='font-size: 18px;'>≡</span> " : "<span style='font-weight: bold'>"}${title}</span>` +
-                `<div class='rockstarButtons' style='display: block; float: right;'>${main ? "<span class=toggleSide style='padding: 4px'> ⇄ </span><span class=minimize style='padding: 4px'> _ </span>" : ""} ` + 
+        const popup = $(`<div>` +
+                `${main ? "<span class=title><span class=icon>≡</span> " : "<span class=titleBold>"}${title}</span>` +
+                `<div class=rockstarButtons>${main ? "<span class=toggleSide> ⇄ </span><span class=minimize> _ </span>" : ""} ` + 
                 (main ? `<a class='whatsNew'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-wclassth="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" /></svg></a>` : '') +
                 (main ? `<a class='settings'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" /></svg></a> ` : '') + 
                 `<a href='https://gabrielsroka.github.io/rockstar/' target='_blank' rel='noopener'>?</a> ` + 
                 `<a class=close>X</a></div><br><br></div>`)
             .appendTo(document.body);
-        const popupBody = $("<div></div>").appendTo(popup);
+        popup.css({position: 'absolute', zIndex: 1000, top: '60px', maxHeight: 'calc(100% - 28px)', maxWidth: 'calc(100% - 28px)', padding: '8px', margin: '4px', overflow: 'auto', backgroundColor: 'white', border: '1px solid #ddd'});
+        popup.find('.icon').css({fontSize: '18px'});
+        popup.find('.titleBold').css({fontWeight: 'bold'});
+        popup.find('.rockstarButtons').css({display: 'block', float: 'right'});
+        popup.find('.toggleSide, .minimize').css({padding: '4px'});
+        const popupBody = $('<div></div>').appendTo(popup);
         popup.find('.close').click(() => popup.remove());
         if (main) {
             popup.find('.title').click(toggleClosed);
